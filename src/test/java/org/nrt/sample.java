fd30a4e43bb1437fcc819fce21cc463759f81a6a
Veenaraman <veenaraman1998@gmail.com>
2021-10-24 16:02:50 +0530

    riya doneghddwdwidw

--- src/test/java/org/nrt/sample.java
+++ src/test/java/org/nrt/sample.java
@@ -4,5 +4,7 @@ public class sample {
 public static void main(String[] args) {
 	System.err.println("sklre");
 	System.out.println("ghjkj");
+	System.out.println("priyaaaa");
+	System.out.println("priya");
 }
 }
